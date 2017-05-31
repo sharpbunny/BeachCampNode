2093e34a8253e21dabf69cd94f8a79879454d1c5
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-05-31 10:54:50 +0200

    Création de connection.js et ajout des routes

--- server.js
+++ server.js
@@ -1,62 +1,61 @@
 //Instanciation des modules
-var express = require ('express');
+var express = require('express');
 var http = require('http');
 var url = require('url');
 var mysql = require('mysql');
-var connexionDB = require('./Class/ConnexionDB.js');
+var connection = require('./connection.js');
 var Query = require('./Class/Query.js');
 
 
 //Serveur http
-var server = http.createServer(function(req, res){
+var server = http.createServer(function(req, res) {
     var page = url.parse(req.url).pathname;
-    switch(page){
-        
+    switch (page) {
+
         //Permet de tester si les identifiants de l'utilisateur lors de la connexion sont corrects
         case '/connexion':
-           var connection = connexionDB.connectionDatabase();
-           Query.connexionUtilisateur(connection);
-           connection.end();
-        break;
-        
-        //Permet de procéder à l'inscription d'un utilisateur
+            var connection = connexionDB.connectionDatabase();
+            Query.connexionUtilisateur(connection);
+            connection.end();
+            break;
+
+            //Permet de procéder à l'inscription d'un utilisateur
         case '/inscription':
 
-        break;
+            break;
 
-        //Obtiens tous les multis autour de lui
+            //Obtiens tous les multis autour de lui
         case '/getMulti':
-        break;
+            break;
 
-        //Créer un multi
+            //Créer un multi
         case '/addMulti':
-        break;
+            break;
 
-        //Modifier son profil
+            //Modifier son profil
         case '/setProfile':
-        break;
+            break;
 
         case '/insertLevel':
             var niveau = {
-                idNiveau : 5,
-                nomNiveau : "Benjamin",
-                experience : 1000
+                idNiveau: 5,
+                nomNiveau: "Benjamin",
+                experience: 1000
             };
 
             var connection = connexionDB.connectionDatabase();
             Query.insertionNiveau(connection, niveau);
 
-        break;
+            break;
 
-        //Par défaut
+            //Par défaut
         default:
 
-        break;
+            break;
     }
-    
+
 
     res.end();
 });
 
 server.listen(8000);
-
